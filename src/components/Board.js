f76596fc06238b53fb72f4c8fcd1e5c18e76cbf0
La-Min-Htut-Khaung <ookokhant55@gmail.com>
2021-09-13 13:26:18 +0630

    Create Board and Square Components

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Square from "./Square";
+
+const Board = () => {
+  return (
+    <div>
+      <Square value="1" />
+      <Square value="2" />
+      <Square value="3" />
+    </div>
+  );
+};
+
+export default Board;
